bb0b30f6f80a5307256dff7a30ce6442d6aab5f4
stephin007 <stephinreji123@gmail.com>
2020-09-24 01:29:26 +0530

    Added the link names and chnnged the bg color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import {BrowserRouter, Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Route path='/'></Route>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () =>{
     return(
         <nav>
-        <div className="nav-wrapper">
+        <div className="nav-wrapper white">
             <a href="#" className="brand-logo left">Logo</a>
             <ul id="nav-mobile" className="right">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="sass.html">Login</a></li>
+            <li><a href="badges.html">Signup</a></li>
+            <li><a href="collapsible.html">Profile</a></li>
             </ul>
         </div>
         </nav>
