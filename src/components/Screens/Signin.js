c0e3e1e66b600917b89ae7e313dced20def34991
stephin007 <stephinreji123@gmail.com>
2020-09-24 20:26:57 +0530

    Connected the Signin route with the database

--- src/components/Screens/Signin.js
+++ src/components/Screens/Signin.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 import './Signin.css'
 
 const Signin = () =>{
+    const history = useHistory()
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const PostData = () => {
+        // Email Validity Checking
+        if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+            M.toast({html:"INVALID EMAIL",classes:"#c62828 red darken-3"})
+            return
+        }
+        fetch("/signin",{
+            method:'post',
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                password,
+                email
+            })
+        })
+        .then(res=>res.json())  //parsing response to json
+        .then(data=>{
+            console.log(data)
+            if(data.error){
+                M.toast({html: data.error, classes: "#c62828 red darken-3"})
+            }
+            else{
+                M.toast({html: "Signed In Successfully", classes:"#388e3c green darken-2"})
+                history.push('/')
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
     return(
       <div className="mycard">
           <div className="card auth-card input-field">
@@ -11,13 +47,21 @@ const Signin = () =>{
             <input 
                 type="text"
                 placeholder="Email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
             />
             <input 
                 type="text"
                 placeholder="Password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
             />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-2">
-                Login
+            <button 
+                className="btn waves-effect waves-light #64b5f6 blue darken-2"
+                onClick={()=>PostData()}
+
+            >
+                Signin
             </button>
 
             <h5>
--- src/components/Screens/Signup.js
+++ src/components/Screens/Signup.js
@@ -1,28 +1,44 @@
 import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 import './Signup.css'
 
-const Signin = () =>{
+const Signup = () =>{
+    const history = useHistory()
     const [name, setName] = useState('')
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
 
     const PostData = () => {
+        // Email Validity Checking
+        if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+            M.toast({html:"INVALID EMAIL",classes:"#c62828 red darken-3"})
+            return
+        }
         fetch("/signup",{
             method:'post',
             headers:{
                 "Content-Type": "application/json"
             },
-            body:JSON.stringify({
-                name:'',
-                password:'',
-                email:''
+            body: JSON.stringify({
+                name,
+                password,
+                email
             })
         })
-        .then(res=>res.json)  //parsing response to json
+        .then(res=>res.json())  //parsing response to json
         .then(data=>{
-            console.log(data)
+            if(data.error){
+                M.toast({html: data.error, classes: "#c62828 red darken-3"})
+            }
+            else{
+                M.toast({html: data.message, classes:"#388e3c green darken-2"})
+                history.push('/signin')
+            }
+        })
+        .catch(err=>{
+            console.log(err)
         })
     }
 
@@ -64,4 +80,4 @@ const Signin = () =>{
     )
 }
 
-export default Signin
+export default Signup
