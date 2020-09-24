449e016613e8608b7222d0b8c8cc86fe1ed6cd4e
stephin007 <stephinreji123@gmail.com>
2020-09-24 18:15:53 +0530

    Added Home Page UI

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,9 +1,70 @@
 import React from 'react'
 
+import './Home.css'
+
 const Home = () =>{
     return(
       <div>
-          <h1>Hello Home</h1>
+          <div className="home">
+            <div className="card home-card">
+              <h5>Stephin Reji</h5>
+              <div className="card-image">
+                <img 
+                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+                  alt=""
+                />
+              </div>
+              <div className="card-content">
+              <i class="material-icons">favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing post</p>
+                <input 
+                  type="text"
+                  placeholder="Add a comment"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="home">
+            <div className="card home-card">
+              <h5>Stephin Reji</h5>
+              <div className="card-image">
+                <img 
+                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+                  alt=""
+                />
+              </div>
+              <div className="card-content">
+              <i class="material-icons">favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing post</p>
+                <input 
+                  type="text"
+                  placeholder="Add a comment"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="home">
+            <div className="card home-card">
+              <h5>Stephin Reji</h5>
+              <div className="card-image">
+                <img 
+                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+                  alt=""
+                />
+              </div>
+              <div className="card-content">
+              <i class="material-icons">favorite</i>
+                <h6>Title</h6>
+                <p>This is an amazing post</p>
+                <input 
+                  type="text"
+                  placeholder="Add a comment"
+                />
+              </div>
+            </div>
+          </div>
       </div>  
     )
 }
