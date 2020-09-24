f97bfac69532dac9b4034907919783473cf5162e
stephin007 <stephinreji123@gmail.com>
2020-09-24 19:11:22 +0530

    Connecting signup route to server:heavy_check_sign:

--- src/components/Screens/Signup.js
+++ src/components/Screens/Signup.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 import './Signup.css'
 
 const Signin = () =>{
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const PostData = () => {
+        fetch("/signup",{
+            method:'post',
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                name:'',
+                password:'',
+                email:''
+            })
+        })
+        .then(res=>res.json)  //parsing response to json
+        .then(data=>{
+            console.log(data)
+        })
+    }
+
     return(
       <div className="mycard">
           <div className="card auth-card input-field">
@@ -11,16 +33,25 @@ const Signin = () =>{
             <input 
                 type="text"
                 placeholder="Name"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
             />
             <input 
                 type="text"
                 placeholder="Email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
             />
             <input 
                 type="text"
                 placeholder="Password"
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
             />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-2">
+            <button 
+                className="btn waves-effect waves-light #64b5f6 blue darken-2"
+                onClick={()=>PostData()}
+            >
                 Sign Up
             </button>
 
