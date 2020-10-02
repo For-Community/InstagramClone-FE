a52255b0c39663c7fd10b2c76358f9ccf3bf94e6
Restyled.io <commits@restyled.io>
2020-10-02 17:08:01 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {createContext, useEffect, useReducer} from 'react';
-import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom'
+import React, { createContext, useEffect, useReducer } from "react";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 
-import Navbar from './components/Navbar'
-import CreatePost from './components/Screens/CreatePost'
-import Home from './components/Screens/Home'
-import Profile from './components/Screens/Profile'
-import Signin from './components/Screens/Signin'
-import Signup from './components/Screens/Signup'
-import {initialState, reducer} from './reducer/userReducer'
+import Navbar from "./components/Navbar";
+import CreatePost from "./components/Screens/CreatePost";
+import Home from "./components/Screens/Home";
+import Profile from "./components/Screens/Profile";
+import Signin from "./components/Screens/Signin";
+import Signup from "./components/Screens/Signup";
+import { initialState, reducer } from "./reducer/userReducer";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 // We need to acess the history so that we can route the user to the specific
 // pages accordingly
@@ -17,38 +17,52 @@ export const UserContext = createContext()
 // routes are inside the browser router. So we will make a sperate component and
 // then call it in the App
 
-const Routing =
-    () => {
-      const history = useHistory()
-      useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"))
-        if (user) {
-          history.push('/')
-        }
-        else {
-          history.push('/signin')
-        }
-      }, [])
-
-      return (<Switch><Route exact path = '/'><Home /><
-              /Route>
-      <Route path='/signin'> <Signin /><
-              /Route>
-      <Route path='/signup'> <Signup /><
-              /Route>
-      <Route path='/profile'> <Profile /><
-              /Route>
-      <Route path='/create'> <CreatePost />
-              </Route>
-    </Switch>)
+const Routing = () => {
+  const history = useHistory();
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      history.push("/");
+    } else {
+      history.push("/signin");
     }
+  }, []);
 
-function
-App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  return (<UserContext.Provider value = {{ state, dispatch }}><BrowserRouter>
-          <Navbar /><Routing /></BrowserRouter>
-      </UserContext.Provider>);
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/signin">
+        {" "}
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        {" "}
+        <Signup />
+      </Route>
+      <Route path="/profile">
+        {" "}
+        <Profile />
+      </Route>
+      <Route path="/create">
+        {" "}
+        <CreatePost />
+      </Route>
+    </Switch>
+  );
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Screens/Signin.js
+++ src/components/Screens/Signin.js
@@ -1,78 +1,77 @@
-import './Signin.css'
+import "./Signin.css";
 
-import M from 'materialize-css'
-import React, {useContext, useState} from 'react'
-import {Link, useHistory} from 'react-router-dom'
+import M from "materialize-css";
+import React, { useContext, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import {UserContext} from '../../App'
+import { UserContext } from "../../App";
 
 const Signin = () => {
-    const {state, dispatch} = useContext(UserContext)
-    const history = useHistory()
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const PostData = () => {
+  const PostData = () => {
+    fetch("/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json()) //parsing response to json
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          return M.toast({ html: data.error, classes: "#c62828 red darken-3" });
+        } else {
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("User", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
+          M.toast({
+            html: "Signed In Successfully",
+            classes: "#388e3c green darken-2",
+          });
+          history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-2"
+          onClick={() => PostData()}
+        >
+          Signin
+        </button>
 
-        fetch("/signin",{
-            method:'post',
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                password,
-                email
-            })
-        })
-        .then(res=>res.json())  //parsing response to json
-        .then(data=>{
-            console.log(data)
-            if(data.error){
-                return M.toast({html: data.error, classes: "#c62828 red darken-3"})
-            }
-            else{
-                localStorage.setItem("jwt", data.token)
-                localStorage.setItem("User", JSON.stringify(data.user))
-                dispatch({type:"USER", payload:data.user})
-                M.toast({html: "Signed In Successfully", classes:"#388e3c green darken-2"})
-                history.push('/')
-            }
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    }
-    return(
-      <div className="mycard">
-          <div className="card auth-card input-field">
-            <h2>Instagram</h2>
-            <input 
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-            />
-            <input 
-                type="text"
-                placeholder="Password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-            />
-            <button 
-                className="btn waves-effect waves-light #64b5f6 blue darken-2"
-                onClick={()=>PostData()}
+        <h5>
+          <Link to="/signup">Don't have an account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
 
-            >
-                Signin
-            </button>
-
-            <h5>
-                <Link to="/signup">Don't have an account?</Link>
-            </h5>
-
-        </div>
-      </div>  
-    )
-}
-
-export default Signin
+export default Signin;
