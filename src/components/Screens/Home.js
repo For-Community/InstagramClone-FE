20cc1023b82a84aa13bc0e479331e380f261ff08
Restyled.io <commits@restyled.io>
2020-10-05 19:27:08 +0000

    Restyled by prettier

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,50 +1,63 @@
-import './Home.css'
+import "./Home.css";
 
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-   const [posts, setposts] = useState({
-     loadedPosts:[
-       {user:'Stephin Reji',profile:'https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        post:'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
-        {user:'Stephin Reji',profile:'https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        post:'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
-        {user:'Stephin Reji',profile:'https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        post:'https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'}
-     ]
-   })
-    return(
-      <div>
+  const [posts, setposts] = useState({
+    loadedPosts: [
       {
-        posts.loadedPosts.map(post =>(
-          <div className="home">
-            <div className="card home-card">
-              <Link to='/profile'><h5> {post.user} </h5></Link>
-              <div className="card-image">
-                <img 
-                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-                  alt=""
-                />
-              </div>
-              <div className="card-content">
-              <i className="material-icons like" title='Like'>favorite</i>
-                <h6>Title</h6>
-                <p>This is an amazing post</p>
-                <div className='add-comment'>
-                    <input 
-                      type="text"
-                      placeholder="Add a comment"
-                    />
-                   
-                 </div>
+        user: "Stephin Reji",
+        profile:
+          "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        post:
+          "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        user: "Stephin Reji",
+        profile:
+          "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        post:
+          "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      },
+      {
+        user: "Stephin Reji",
+        profile:
+          "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        post:
+          "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      },
+    ],
+  });
+  return (
+    <div>
+      {posts.loadedPosts.map((post) => (
+        <div className="home">
+          <div className="card home-card">
+            <Link to="/profile">
+              <h5> {post.user} </h5>
+            </Link>
+            <div className="card-image">
+              <img
+                src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+            </div>
+            <div className="card-content">
+              <i className="material-icons like" title="Like">
+                favorite
+              </i>
+              <h6>Title</h6>
+              <p>This is an amazing post</p>
+              <div className="add-comment">
+                <input type="text" placeholder="Add a comment" />
               </div>
             </div>
           </div>
-        ))
-      }
-      </div>  
-    )
-}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Screens/Profile.js
+++ src/components/Screens/Profile.js
@@ -1,73 +1,85 @@
-import './Profile.css'
+import "./Profile.css";
 
-import React from 'react'
+import React from "react";
 
-const Profile =
-    () => {
-    return(
-      <div style={{
-    maxWidth: "550px", margin: "0px auto"
-      }}>
-          <div style={{
-    display: "flex", justifyContent: "space-around", margin: "18px 0px",
-        borderBottom: "1px solid grey"
-          }}>
-            <div>
-              <img
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    alt = ""
-    style =
-    {
-      { width: "160px", height: "160px", borderRadius: "80px" }
-    }
+const Profile = () => {
+  return (
+    <div
+      style={{
+        maxWidth: "550px",
+        margin: "0px auto",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px 0px",
+          borderBottom: "1px solid grey",
+        }}
+      >
+        <div>
+          <img
+            src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+          />
+        </div>{" "}
+        <div>
+          <h4>Stephin Reji</h4>
 
-    />
-            </div > <div>
-        <h4>Stephin Reji<
-            /h4>
-              
-              <div 
-              style={{
-                display:"flex", 
-                justifyContent:"space-between",
-                width:"108%"
-              }}>
-                <h5>40 posts</h5>
-        <h5>40 Followers</h5>
-                <h5>40 Following</h5>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%",
+            }}
+          >
+            <h5>40 posts</h5>
+            <h5>40 Followers</h5>
+            <h5>40 Following</h5>
+          </div>
         </div>
-            </div><
-        /div>
-          
-          <div className="gallery">
-            <img className="item"
-              src="https:/ /
+      </div>
+
+      <div className="gallery">
+        <img
+          className="item"
+          src="https:/ /
                 images.unsplash.com / photo -
             1550927407 - 50e2bd128b81
         ? ixlib = rb - 1.2.1 &ixid = eyJhcHBfaWQiOjEyMDd9 &auto = format &fit =
-                      crop &w = 500 &q = 60 " 
-    alt = "" / > < img className = "item"
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    alt = "" / > < img className = "item"
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    alt = "" / > < img className = "item"
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    alt = "" / > < img className = "item"
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    alt = "" / > < img className = "item"
-    src =
-        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-              alt=""
-            />
-          </div>
+                      crop &w = 500 &q = 60 "
+          alt=""
+        />{" "}
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />{" "}
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />{" "}
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />{" "}
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />{" "}
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-      </div>  
-    )
-    }
-
-export default Profile
+export default Profile;
