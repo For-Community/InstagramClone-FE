b7535e4e8d286330fe6fdca80b032e9a2a28d3a6
Restyled.io <commits@restyled.io>
2020-10-05 19:27:07 +0000

    Restyled by clang-format

--- src/components/Screens/Profile.js
+++ src/components/Screens/Profile.js
@@ -1,28 +1,31 @@
-import React from 'react'
-
 import './Profile.css'
 
-const Profile = () =>{
+import React from 'react'
+
+const Profile =
+    () => {
     return(
-      <div style={{maxWidth:"550px", 
-        margin:"0px auto"
+      <div style={{
+    maxWidth: "550px", margin: "0px auto"
       }}>
           <div style={{
-            display:"flex",
-            justifyContent:"space-around",
-            margin: "18px 0px",
-            borderBottom:"1px solid grey"
+    display: "flex", justifyContent: "space-around", margin: "18px 0px",
+        borderBottom: "1px solid grey"
           }}>
             <div>
-              <img 
-                src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-                alt="" 
-                style={{width:"160px", height:"160px", borderRadius:"80px"}}
+              <img
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    alt = ""
+    style =
+    {
+      { width: "160px", height: "160px", borderRadius: "80px" }
+    }
 
-              />
-            </div>
-            <div>
-              <h4>Stephin Reji</h4>
+    />
+            </div > <div>
+        <h4>Stephin Reji<
+            /h4>
               
               <div 
               style={{
@@ -31,41 +34,40 @@ const Profile = () =>{
                 width:"108%"
               }}>
                 <h5>40 posts</h5>
-                <h5>40 Followers</h5>
+        <h5>40 Followers</h5>
                 <h5>40 Following</h5>
-              </div>
-            </div>
-          </div>
+        </div>
+            </div><
+        /div>
           
           <div className="gallery">
             <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-              alt=""
-            />
-            <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-              alt=""
-            />
-            <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-              alt=""
-            />
-            <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-              alt=""
-            />
-            <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-              alt=""
-            />
-            <img className="item"
-              src="https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
+              src="https:/ /
+                images.unsplash.com / photo -
+            1550927407 - 50e2bd128b81
+        ? ixlib = rb - 1.2.1 &ixid = eyJhcHBfaWQiOjEyMDd9 &auto = format &fit =
+                      crop &w = 500 &q = 60 " 
+    alt = "" / > < img className = "item"
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    alt = "" / > < img className = "item"
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    alt = "" / > < img className = "item"
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    alt = "" / > < img className = "item"
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    alt = "" / > < img className = "item"
+    src =
+        "https://images.unsplash.com/photo-1550927407-50e2bd128b81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
               alt=""
             />
           </div>
 
       </div>  
     )
-}
+    }
 
 export default Profile
--- src/components/Screens/Signin.js
+++ src/components/Screens/Signin.js
@@ -1,45 +1,43 @@
-import React, {useState, useContext} from 'react'
-import {Link, useHistory} from 'react-router-dom'
-import {UserContext} from '../../App'
-import M from 'materialize-css'
-
 import './Signin.css'
 
-const Signin = () =>{
-    const {state, dispatch} = useContext(UserContext)
-    const history = useHistory()
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
+import M from 'materialize-css'
+import React, {useContext, useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+
+import {UserContext} from '../../App'
+
+const Signin =
+    () => {
+      const {state, dispatch} = useContext(UserContext)
+      const history = useHistory()
+      const [password, setPassword] = useState('')
+      const [email, setEmail] = useState('')
 
     const PostData = () => {
-
-        fetch("/signin",{
-            method:'post',
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                password,
-                email
+    fetch("/signin", {
+      method : 'post',
+      headers : {"Content-Type" : "application/json"},
+      body : JSON.stringify({password, email})
+    })
+        .then(res => res.json()) // parsing response to json
+        .then(data => {
+          console.log(data)
+          if (data.error) {
+            return M.toast(
+                {html : data.error, classes : "#c62828 red darken-3"})
+          }
+          else {
+            localStorage.setItem("jwt", data.token)
+            localStorage.setItem("User", JSON.stringify(data.user))
+            dispatch({type : "USER", payload : data.user})
+            M.toast({
+              html : "Signed In Successfully",
+              classes : "#388e3c green darken-2"
             })
+            history.push('/')
+          }
         })
-        .then(res=>res.json())  //parsing response to json
-        .then(data=>{
-            console.log(data)
-            if(data.error){
-                return M.toast({html: data.error, classes: "#c62828 red darken-3"})
-            }
-            else{
-                localStorage.setItem("jwt", data.token)
-                localStorage.setItem("User", JSON.stringify(data.user))
-                dispatch({type:"USER", payload:data.user})
-                M.toast({html: "Signed In Successfully", classes:"#388e3c green darken-2"})
-                history.push('/')
-            }
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+        .catch(err => {console.log(err)})
     }
     return(
       <div className="mycard">
@@ -51,30 +49,28 @@ const Signin = () =>{
                 value={email}
                 onChange={(e)=>setEmail(e.target.value)}
             />
-            <input 
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-            />
+            <input
+    type = "password"
+    placeholder = "Password"
+    value = {password} onChange =
+    {
+      (e) => setPassword(e.target.value)
+    } />
             <button 
                 className="btn waves-effect waves-light #64b5f6 blue darken-2"
                 onClick={()=>PostData()}
 
             >
                 Signin
-            </button>
-            <p className='pointer link'>Forgot Password?</p>
-        </div>
-        <Link to='/signup'>
-            <div className='have-account card link'>
-                <h6>
-                    <Link to="/signup">Don't have an account? <span className='link'>Sign Up</span> </Link>
-                </h6>
-            </div>
-        </Link>
-      </div>  
-    )
-}
+            </button >
+        <p className = 'pointer link'>Forgot Password
+        ? </p>
+        </div><Link to = '/signup'>
+        <div className = 'have-account card link'><h6><Link to = "/signup">Don
+        't have an account? <span className=' link'>Sign Up</span> </Link> <
+            /h6>
+            </div></Link>
+      </div>)
+    }
 
 export default Signin
