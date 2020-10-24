abc586df2a268c3b496610ab5849ca97c44437ca
Stephin Reji <stephinreji123@gmail.com>
2020-10-24 23:58:57 +0530

    Fetch post from db (#15)
    * Added a check for user if goes offline but has not logged out:heavy_check_sign:
    
    * Changed the navbar look if the user is not signedin
    
    * Added check to stop user to get to home page when clicked on Insta Logo
    
    * Fetched data from DB but data is still not rendred
    
    * rendered the data in the front end

--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,69 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './Home.css'
 
 const Home = () =>{
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('/allpost',{
+      headers:{
+        "Authorization": "Bearer "+localStorage.getItem("jwt")
+      }
+    })
+      .then(res=>res.json())
+      .then(result=>{
+        console.log(result)
+        setData(result.posts)
+      })
+  },[])
     return(
       <div>
           <div className="home">
-            <div className="card home-card">
-              <h5>Stephin Reji</h5>
-              <div className="card-image">
-                <img 
-                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-                  alt=""
-                />
-              </div>
-              <div className="card-content">
-              <i className="material-icons">favorite</i>
-                <h6>Title</h6>
-                <p>This is an amazing post</p>
-                <input 
-                  type="text"
-                  placeholder="Add a comment"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="home">
-            <div className="card home-card">
-              <h5>Stephin Reji</h5>
-              <div className="card-image">
-                <img 
-                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-                  alt=""
-                />
-              </div>
-              <div className="card-content">
-              <i className="material-icons">favorite</i>
-                <h6>Title</h6>
-                <p>This is an amazing post</p>
-                <input 
-                  type="text"
-                  placeholder="Add a comment"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="home">
-            <div className="card home-card">
-              <h5>Stephin Reji</h5>
-              <div className="card-image">
-                <img 
-                  src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
-                  alt=""
-                />
-              </div>
-              <div className="card-content">
-              <i className="material-icons">favorite</i>
-                <h6>Title</h6>
-                <p>This is an amazing post</p>
-                <input 
-                  type="text"
-                  placeholder="Add a comment"
-                />
-              </div>
-            </div>
+            {
+              data.map(item=>{
+                return(
+                  <div className="card home-card" key={item._id} >
+                    <h5>{item.postedBy.name}</h5>
+                    <div className="card-image">
+                      <img 
+                        src={item.photo} 
+                        alt="User Uploaded Image"
+                      />
+                    </div>
+                    <div className="card-content">
+                    <i className="material-icons">favorite</i>
+                      <h6>{item.title}</h6>
+                      <p>{item.body}</p>
+                      <input 
+                        type="text"
+                        placeholder="Add a comment"
+                      />
+                    </div>
+                  </div>
+                )
+              })
+            }
+            
           </div>
       </div>  
     )
