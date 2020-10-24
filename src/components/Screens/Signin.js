b2236d1d3a8637d7e7c7cf4b10011eae2be24da0
stephin007 <stephinreji123@gmail.com>
2020-10-25 00:21:23 +0530

    Chnged the input type to password for signin

--- src/components/Screens/Signin.js
+++ src/components/Screens/Signin.js
@@ -52,7 +52,7 @@ const Signin = () =>{
                 onChange={(e)=>setEmail(e.target.value)}
             />
             <input 
-                type="text"
+                type="password"
                 placeholder="Password"
                 value={password}
                 onChange={(e)=>setPassword(e.target.value)}
