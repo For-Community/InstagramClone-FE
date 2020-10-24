7aee5f99ed63bd346e685eb73e920e4f8168a608
stephin007 <stephinreji123@gmail.com>
2020-10-25 00:23:55 +0530

    Chnged the input type to password for signup

--- src/components/Screens/Signup.js
+++ src/components/Screens/Signup.js
@@ -54,7 +54,7 @@ const Signup = () =>{
                 onChange={(e)=>setEmail(e.target.value)}
             />
             <input 
-                type="text"
+                type="password"
                 placeholder="Password"
                 value={password}
                 onChange={(e)=>setPassword(e.target.value)}
